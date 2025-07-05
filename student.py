534973128f13cce2f4d14aa616cb1466e048065f
Victor Marin <victormarinviloria@gmail.com>
2025-07-05 09:03:54 +0200

    Add files via upload

--- student.py
+++ student.py
@@ -1,11 +1,20 @@
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor, Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
+    print(f'{student.name} from {student.house}')
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return (name, house)
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
